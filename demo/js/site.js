d35912d24473346950a79290d0b24473ababf149
Hakan Bilgin <hbi99@hotmail.com>
2026-03-28 23:07:46 +0100

    ux

--- demo/js/site.js
+++ demo/js/site.js
@@ -4,6 +4,12 @@
 const App = {
 	init() {
 		Resize.init();
+	},
+	dispatch(event) {
+		switch (event.type) {
+			case "resize-layout":
+				break;
+		}
 	}
 };
 
@@ -30,40 +36,49 @@ let Resize = {
 				// locked divider
 				if (org.prop("className").includes("locked")) return;
 				// collect info
-				let offset = org.offset(),
-					el = org.clone().addClass("clone").css(offset),
+				let off = org.offset(),
+					el = org.clone().addClass("clone").css(off),
 					click = {
-						x : event.clientX - offset.left - event.offsetX - 14,
-						y : event.clientY - offset.top - event.offsetY - 7,
+						x : event.clientX - off.left - event.offsetX - 14,
+						y : event.clientY - off.top - event.offsetY - 14,
 					},
 					min = {
-						x: 0,
-						y: 0
+						x: 440,
+						y: 500
 					},
 					max = {
-						x: 1e3,
-						y: 1e3
+						x: 950,
+						y: 1050
 					};
 				// insert clone in to DOM and cover body
 				Resize.els.body.addClass("covered").append(el);
 				el = Resize.els.body.find(".clone");
 				// safe drag details
-				Resize.drag = { el, dir, click, min, max };
+				Resize.drag = { el, org, dir, click, off, min, max };
 				// bind more event handlers
 				Resize.els.doc.bind("mousemove mouseup", Resize.dispatch);
 				break;
 			case "mousemove":
 				switch (Drag.dir) {
 					case "vertical":
-						data.left = Math.max(Drag.min.x, event.clientX + Drag.click.x);
+						data.left = Math.min(Drag.max.x, Math.max(Drag.min.x, event.clientX + Drag.click.x));
 						break;
 					case "horisontal":
-						data.top = Math.max(Drag.min.y, event.clientY + Drag.click.y);
+						data.top = Math.min(Drag.max.y, Math.max(Drag.min.y, event.clientY + Drag.click.y));
 						break;
 				}
 				Drag.el.css(data);
 				break;
 			case "mouseup":
+				let offset = Drag.el.offset(),
+					bEl = Drag.org.prevAll("div:first"),
+					aEl = Drag.org.nextAll("div:first"),
+					diff = offset.left - Drag.off.left,
+					bW = bEl.width() + diff - 7,
+					aW = aEl.width() - diff - 7;
+				bEl.css({ width: bW });
+				aEl.css({ width: aW });
+
 				// delete clone from DOM
 				Drag.el.remove();
 				// reset body
