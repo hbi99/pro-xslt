55ceb1efc744f5b2fa5ff4890d1b9be810de6089
Hakan Bilgin <hbi99@hotmail.com>
2026-06-30 10:06:34 +0200

    interface functions

---
+++ tests/interace.js
@@ -0,0 +1,39 @@
+import { describe, it, expect } from 'vitest';
+import ProXslt from '../src/index.js';
+import { loadXml } from './utils/common.js';
+
+describe('Interface Tests', () => {
+
+    it('should use setParameter to interface values of parameters.', async () => {
+        let { xmlDoc, xslDoc } = loadXml(`interface/setPArameter.xml`);
+        let proXslt = new ProXslt();
+        proXslt.importStylesheet(xslDoc);
+        proXslt.setParameter(null, "highlightColor", "lightblue");
+        let fragment = proXslt.transformToFragment(xmlDoc, document);
+
+        expect(fragment.textContent.trim()).toBe(`<ul><li style="background-color: lightblue;">Item 1</li></ul>`);
+    });
+
+    it('should use getParameter to get value of parameter.', async () => {
+        let { xmlDoc, xslDoc } = loadXml(`interface/setPArameter.xml`);
+        let proXslt = new ProXslt();
+        proXslt.importStylesheet(xslDoc);
+        let show = proXslt.getParameter(null, "showItems");
+        let color = proXslt.getParameter(null, "highlightColor");
+        
+        expect(show).toBe(`yes`);
+        expect(color).toBe(`yellow`);
+    });
+
+    it('should clear parameters with clearParameter.', async () => {
+        let { xmlDoc, xslDoc } = loadXml(`interface/setPArameter.xml`);
+        let proXslt = new ProXslt();
+        proXslt.importStylesheet(xslDoc);
+        proXslt.setParameter(null, "highlightColor", "lightblue");
+        proXslt.clearParameters();
+        let fragment = proXslt.transformToFragment(xmlDoc, document);
+
+        expect(fragment.textContent.trim()).toBe(`<ul><li style="background-color: yellow;">Item 1</li></ul>`);
+    });
+
+});
